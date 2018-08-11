26ef3332900b98db7f58f0d69108b2a2f1705b0a
sergioalmela <sergioalmela@gmail.com>
2018-08-11 15:22:36 +0200

    Agregada primera página PHP

---
+++ primero.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>Nuestra primera página web en PHP con ElSoftwareLibre</title>
+  <style>
+   .primera_web {
+    font_size: 16px;
+   }
+  </style>
+ </head>
+ <body>
+  <p class="primera_web"><strong><?php echo "Mi primera página web"; ?></strong></p>
+ </body>
+</html>
